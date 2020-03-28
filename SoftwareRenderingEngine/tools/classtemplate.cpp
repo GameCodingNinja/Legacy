bbc48a3057510cb7a8f1ac6a96272fbe70e3eea5
GameCodingNinja <hrosenorn@gmail.com>
2020-03-27 20:20:21 -0500

    initial version

---
+++ SoftwareRenderingEngine/tools/classtemplate.cpp
@@ -0,0 +1,38 @@
+
+/************************************************************************
+*    FILE NAME:       classtemplate.cpp
+*
+*    DESCRIPTION:     Class template
+************************************************************************/
+
+// Physical component dependency
+#include <utilities/classtemplate.h>
+
+// Standard lib dependencies
+
+// Boost lib dependencies
+
+// Game lib dependencies
+
+// Game dependencies
+
+
+/************************************************************************
+*    desc:  Constructor
+************************************************************************/
+CClassTemplate::CClassTemplate()
+{
+}   // constructor
+
+
+/************************************************************************
+*    desc:  destructor                                                             
+************************************************************************/
+CClassTemplate::~CClassTemplate()
+{
+}	// destructor
+
+
+
+
+
